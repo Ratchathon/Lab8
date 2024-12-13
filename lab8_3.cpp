7ddffce6f438e83ab5591f10db9baaede5e87fdc
Ratchathon <Ratchathon_S@cmu.ac.th>
2024-12-13 23:16:19 +0700

    lab8.1 done

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,54 @@
 "Brook"
 "Jinbe"
 */
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int age,tall;
+    long long int bounty;
+    string name;
+    cout<<"Enter your age: ";
+    cin>>age;
+    if(age <= 25)
+    {
+     cout<<"Enter your height: ";
+     cin>>tall;
+     if(tall < 100)
+     {
+        name = "Chopper";
+     } else{
+        if(tall < 180)
+        {
+            name = "Usopp";
+        }else{
+            cout<<"Enter your bounty: ";
+            cin>>bounty;
+            if(bounty > 1100000000)
+            {
+                name = "Zoro";
+            }else{
+                name = "Sanji";
+            }
+        }
+     }
+    }else
+    {
+        if(age <= 60)
+        {
+            cout<<"Enter your bounty: ";
+            cin>>bounty;
+            if(bounty > 500000000)
+            {
+                name = "Jinbe";
+            }else{
+                name = "Franky";
+            }
+        }else{
+            name = "Brook";
+        }
+    }cout<<"Your character = "<<name;
+}
